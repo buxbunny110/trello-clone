96d923eab499e30ad3e3d5cc6088e4c2167c35da
Irakli <ekeushka@gmail.com>
2023-06-18 18:56:14 +0400

    create all styled components, create containers and assemble them together

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
-import { Button } from './styles';
+import { AppContainer } from './styles';
+import {Column} from "./Column";
+import {Card} from "./Card";
 function App() {
-  return <Button>Regular Button</Button>
+  return (
+      <AppContainer>
+        <Column title="First Col">
+          <Card text="First Card"/>
+        </Column>
+      </AppContainer>
+  )
 }
 
 export default App;
---
+++ src/Column.tsx
@@ -0,0 +1,15 @@
+import { ColumnContainer, ColumnTitle } from "./styles";
+import {PropsWithChildren} from "react";
+
+type ColumnProps = PropsWithChildren<{
+  title: string
+}>
+
+export const Column = ({ title, children }: ColumnProps) => {
+  return (
+    <ColumnContainer>
+      <ColumnTitle>{title}</ColumnTitle>
+      {children}
+    </ColumnContainer>
+  )
+}
